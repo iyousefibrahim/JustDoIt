da90d78ea707a77c10caa8baf7cc55c08d8b332d
Yousef <ioyx515@gmail.com>
2024-09-21 15:44:41 +0300

    Added five new methods to TodoService: addTodo, markCompleted, deleteTodo, getAllTodos, and getApiKey, which make HTTP requests to API endpoints.

--- src/app/core/services/todo.service.ts
+++ src/app/core/services/todo.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +8,33 @@ import { Injectable } from '@angular/core';
 export class TodoService {
 
   constructor(private _HttpClient: HttpClient) { }
+
+  addTodo(title: string, apiKey: string): Observable<any> {
+    return this._HttpClient.post(baseUrl + 'api/v1/todos', {
+      "title": title,
+      "apiKey": apiKey
+    })
+  }
+
+  markCompleted(todoId: string): Observable<any> {
+    return this._HttpClient.put(baseUrl + 'api/v1/todos', {
+      "todoId": todoId,
+    })
+  }
+
+  deleteTodo(todoId: string): Observable<any> {
+    return this._HttpClient.delete(baseUrl + 'api/v1/todos', {
+      body: {
+        "todoId": todoId
+      }
+    })
+  }
+
+  getallTodos(todoId: string): Observable<any> {
+    return this._HttpClient.get(baseUrl + `api/v1/todos/${todoId}`)
+  }
+
+  getApiKey(): Observable<any> {
+    return this._HttpClient.get(baseUrl + `api/v1/getApiKey`)
+  }
 }
