2916fef381170b745aca06a9229c5b108ca48c83
Yousef <ioyx515@gmail.com>
2024-09-21 15:34:20 +0300

    Set up SCSS file with site color variables and fonts, installed ngx-toastr, added two layouts (Main and Start), set up routing for the layouts, and added core components (Welcome, Not Found, Home).

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { StartLayoutComponent } from './layouts/start-layout/start-layout.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: "", component: StartLayoutComponent, children: [
+            { path: "", redirectTo: "welcome", pathMatch: "full" },
+            { path: "welcome", component: WelcomeComponent, title: "Welcome" }
+        ]
+    },
+    {
+        path: "", component: MainLayoutComponent, children: [
+            { path: "", redirectTo: "home", pathMatch: "full" },
+            { path: "todos", component: HomeComponent, title: "My Todos" }
+        ]
+    },
+    { path: '**', component: NotfoundComponent, title: "Not Found 404" }
+
+];
